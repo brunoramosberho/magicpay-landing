d27e0279096b200dd5cb6a0e0dc4dcfedc93aae1
Bruno Ramos Berho <brunoramosberho@me.com>
2025-10-30 10:02:23 -0600

    feat: add es-MX copy and fix i18n build issues

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,18 +1,19 @@
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
 
-export default getRequestConfig(async ({requestLocale}) => {
-  // This typically corresponds to the `[locale]` segment
+export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
 
-  // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (
+    typeof locale !== 'string' ||
+    !routing.locales.some((supportedLocale) => supportedLocale === locale)
+  ) {
     locale = routing.defaultLocale;
   }
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
 
